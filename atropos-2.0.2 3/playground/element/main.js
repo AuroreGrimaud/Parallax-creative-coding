9978c796ccd4bf49e1ad456b41d8af27ec3f29af
AuroreGrimaud <aurore@famillegrimaud.fr>
2024-11-09 19:38:42 +0100

    Update main.js

--- atropos-2.0.2 3/playground/element/main.js
+++ atropos-2.0.2 3/playground/element/main.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line
+import Atropos from 'https://cdn.jsdelivr.net/npm/atropos@2/atropos.min.mjs';
 import AtroposComponent from '../../build/atropos-element.mjs';
 
 customElements.define('atropos-component', AtroposComponent);

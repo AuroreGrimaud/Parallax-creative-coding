b140f00c048fdcdceeca2a544e6c3331ea4bb841
AuroreGrimaud <aurore@famillegrimaud.fr>
2024-11-13 12:50:08 +0100

    Update main.js anim descriptions

--- atropos-2.0.2 3/playground/element/main.js
+++ atropos-2.0.2 3/playground/element/main.js
@@ -3,3 +3,21 @@ import Atropos from 'https://cdn.jsdelivr.net/npm/atropos@2/atropos.min.mjs';
 import AtroposComponent from '../../build/atropos-element.mjs';
 
 customElements.define('atropos-component', AtroposComponent);
+
+
+function openModal() {
+    document.getElementById("projectModal").style.display = "block";
+}
+
+// Fonction pour fermer la modal
+function closeModal() {
+    document.getElementById("projectModal").style.display = "none";
+}
+
+// Fermer la modal si on clique en dehors du contenu de la modal
+window.onclick = function(event) {
+    const modal = document.getElementById("projectModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
